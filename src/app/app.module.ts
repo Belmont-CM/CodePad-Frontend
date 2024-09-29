c29159a3dd8bf24afba1efd603164ec7d31a906f
zeuscm <zeuscm.work@gmail.com>
2024-09-28 18:52:31 -0600

    ADD - Se agrega Tailwind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faLock, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
@@ -26,6 +26,6 @@ import { LoginComponent } from './login/login.component';
 })
 export class AppModule {
   constructor(library: FaIconLibrary) {
-    library.addIcons(faEye, faEyeSlash);
+    library.addIcons(faUser, faLock, faEye, faEyeSlash);
   }
 }
