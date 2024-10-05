eccc7c2d439223ba23fce6d895e13ba13792150e
zeuscm <zeuscm.work@gmail.com>
2024-10-04 21:32:49 -0600

    FIX - SETTINGS - FORMULARIO

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,72 +1,91 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {
+  faSearch, faCogs, faUser, faSignOutAlt, faBell, faClipboardList,
+  faMoon, faSun, faGlobe, faCode, faLaptop, faCloud, faBolt, faSliders, faEye, faPalette, faLock
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.css']
 })
-export class SettingsComponent implements OnInit {
-  // General settings
-  darkMode = true;
-  language = 'es';
-  notifications = true;
+export class SettingsComponent {
+  // Iconos
+  faSearch = faSearch;
+  faCogs = faCogs;
+  faUser = faUser;
+  faSignOutAlt = faSignOutAlt;
+  faBell = faBell;
+  faClipboardList = faClipboardList;
+  faMoon = faMoon;
+  faSun = faSun;
+  faGlobe = faGlobe;
+  faCode = faCode;
+  faLaptop = faLaptop;
+  faCloud = faCloud;
+  faBolt = faBolt;
+  faSliders = faSliders;
+  faEye = faEye;
+  faPalette = faPalette;
+  faLock = faLock;
 
-  // Editor settings
-  fontSize = 14;
-  livePreview = true;
+  // Variables para la configuración
+  isSidenavOpen: boolean = true;
+  darkMode: boolean = true;
+  language: string = 'es';
+  notifications: boolean = true;
+  fontSize: number = 14;
+  livePreview: boolean = true;
+  theme: string = 'neon';
+  autoSave: boolean = true;
+  cloudSync: boolean = true;
 
-  // Appearance settings
-  theme = 'neon';
+  // Otras variables existentes
+  settingsOptions: string[] = ['Perfil', 'Notificaciones', 'Preferencias', 'Conexiones'];
+  recentActions: { action: string, time: string }[] = [
+    { action: 'Cambiaste tu foto de perfil', time: 'Hace 1 hora' },
+    { action: 'Modificaste tus preferencias de notificaciones', time: 'Ayer' },
+    { action: 'Actualizaste tu contraseña', time: 'Hace 2 días' },
+    { action: 'Conectaste una nueva cuenta de GitHub', time: 'Hace 3 días' },
+  ];
 
-  // Advanced settings
-  autoSave = true;
-  cloudSync = true;
-
-  // Sidenav state
-  isSidenavOpen = false;
-
-  constructor() { }
-
-  ngOnInit(): void { }
-
-  toggleSidenav(): void {
+  toggleSidenav() {
     this.isSidenavOpen = !this.isSidenavOpen;
   }
 
-  handleLogout(): void {
-    // Implement logout logic here
-  }
-
-  // Methods to handle setting changes
-  toggleDarkMode(): void {
+  toggleDarkMode() {
     this.darkMode = !this.darkMode;
   }
 
-  changeLanguage(lang: string): void {
+  setLanguage(lang: string) {
     this.language = lang;
   }
 
-  toggleNotifications(): void {
+  toggleNotifications() {
     this.notifications = !this.notifications;
   }
 
-  changeFontSize(size: number): void {
+  setFontSize(size: number) {
     this.fontSize = size;
   }
 
-  toggleLivePreview(): void {
+  toggleLivePreview() {
     this.livePreview = !this.livePreview;
   }
 
-  changeTheme(newTheme: string): void {
+  setTheme(newTheme: string) {
     this.theme = newTheme;
   }
 
-  toggleAutoSave(): void {
+  toggleAutoSave() {
     this.autoSave = !this.autoSave;
   }
 
-  toggleCloudSync(): void {
+  toggleCloudSync() {
     this.cloudSync = !this.cloudSync;
+  }
+
+  handleLogout() {
+    console.log('Cerrando sesión...');
   }
 }
